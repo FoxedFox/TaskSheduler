9d6be9d5e5e37127a2c308acba53b89db2e45ac2
FoxedFox <71232184+FoxedFox@users.noreply.github.com>
2021-02-16 11:09:19 +0300

    Fixed condition of order

--- TaskScheduler/TaskScheduler.cpp
+++ TaskScheduler/TaskScheduler.cpp
@@ -7,7 +7,7 @@ int entrytype=0;
 void RandomEnter()
 {
     srand(time(NULL));
-   
+    int workout=0;
     int WorkTime, Quant = 0;  
     const int CountTask = 20;
     int TaskTime[CountTask] = { 0 };
@@ -41,7 +41,7 @@ void RandomEnter()
         printf("%3d|", EnterTaskTime[i]);
     }
     std::cout << "\n_________________________________________________________________________________________|\n";
-    WorkTime = rand() % 80 + 20;
+    WorkTime = rand() % 80 + 2000;
     std::cout << "How much time? There is " << WorkTime << " left\n";
     int* s = new int[CountTask];
     for (int j = 0; j < CountTask; j++)
@@ -49,40 +49,51 @@ void RandomEnter()
         middlePriority[j] = double (TaskPriority[j])/ double(TaskTime[j]);
     }
     for (int j = 0; j < CountTask; j++) s[j] = 0;
-    int sum = 0;
+    
     int i = 1;
     while (Quant < WorkTime)
-    {
+    { 
+        int triggered = 0;
         double mpr = 0;
         int PriorityMax = 0;
         int ChoosedMax = 0;
         int TimeMax = 0;
         for (int j = 0;j < CountTask;j++)
-          
-                if ((middlePriority[j] > PriorityMax) && (s[j] != 1) && (Quant + TaskTime[j] <= WorkTime))
+        {
+            if (EnterTaskTime[j] <= Quant)
+            {
+                if ((middlePriority[j] > mpr) && (s[j] != 1) && (Quant + TaskTime[j] <= WorkTime))
                 {
                     mpr = middlePriority[j];
                     ChoosedMax = Count[j];
                     TimeMax = TaskTime[j];
                     PriorityMax = TaskPriority[j];
                 }
-            
-
+            }
+        }
 
         if (ChoosedMax == 0 && PriorityMax == 0 && TimeMax == 0)
             {
-                std::cout << "\nFULL.\n";
-                break;
+                std::cout << "====+1 Quant.=====\n";
+                Quant++;
+                triggered = 1;
             }
-           
+        
         Quant += TimeMax;
-
+        
+        if (triggered != 1)
+        {
             std::cout << "\n STEP " << i ;
             std::cout << "\nNumber:     " << ChoosedMax << "\nPriority:  " << PriorityMax << "\nTime:       " << TimeMax
                 << "\nTime gone: " << Quant << "\n Middle priority " << mpr<< "\n";
             s[ChoosedMax - 1] = 1;
-
+         workout++;
             i++;
+        }
+        if (workout == 20)
+        {
+            break;
+        }
         }
     std::cout << "\nS: ";
     for (int j = 0; j < CountTask; j++) std::cout << s[j] << " ";
